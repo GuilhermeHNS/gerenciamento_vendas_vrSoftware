6be157ad4d09faac0a2bc3ddabd6adc8c9e5d08e
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-12-22 13:55:08 -0300

    Configurado banco de dados

---
+++ src/main/java/com/GuilhermeHNS/gerenciamento_vendas_vrSoftware/configuration/scriptsDB/baseGerenciamentoVendas.sql
@@ -0,0 +1,22 @@
+CREATE TABLE cliente (
+    cliente_id SERIAL PRIMARY KEY,
+    cliente_name VARCHAR(100) NOT NULL,
+    cliente_cpfCnpj VARCHAR(14) UNIQUE NOT NULL,
+    cliente_limiteCompra NUMERIC(10,2) NOT NULL,
+    cliente_diaFechamentoFatura SMALLINT NOT NULL
+);
+
+CREATE TABLE produto (
+    produto_id SERIAL PRIMARY KEY,
+    produto_descricao VARCHAR(255) NOT NULL,
+    produto_preco NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE vendas (
+    vendas_cliente_id INT NOT NULL,
+    vendas_produto_id INT NOT NULL,
+    vendas_dataVendas TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT pk_vendas PRIMARY KEY (vendas_cliente_id, vendas_produto_id),
+    CONSTRAINT fk_cliente FOREIGN KEY (vendas_cliente_id) REFERENCES cliente(cliente_id),
+    CONSTRAINT fk_produto FOREIGN KEY (vendas_produto_id) REFERENCES produto(produto_id)
+);
